fd2414b15cf8b72a48d92ccf2603007acdf7f585
Andres <pyperan@gmail.com>
2022-02-15 23:11:25 +0100

    añado bucle principal de pygame

---
+++ bucle_principal.py
@@ -0,0 +1,27 @@
+import pygame as pg
+
+pg.init()
+pantalla = pg.display.set_mode((600,800))
+
+game_over = False
+
+while not game_over:
+    eventos = pg.event.get()
+
+    for evento in eventos:
+        if evento.type == pg.QUIT:
+            game_over = True
+
+
+    pantalla.fill((255,0,0))
+    
+    pg.display.flip()
+
+pg.quit()
+
+
+
+
+
+
+
